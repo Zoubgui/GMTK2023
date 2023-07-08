ffa101552d78db28ff2e55b0b36d30ea023f036b
Zoubgui <gui.home.ehrhardt@gmail.com>
2023-07-08 15:56:10 +0200

    Sounds

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,10 +11,21 @@ public class Movement : MonoBehaviour
 
     public SpriteRenderer sprite;
 
+    AudioSource wallSoundEffect;
+    AudioSource trapSoundEffect;
+    AudioSource ennemiSoundEffect;
+
     void Start()
     {
         rb.AddForce(Vector2.down * speed/2);
         rb.AddForce(Vector2.left * speed/3);
+
+        wallSoundEffect = transform.GetChild(1).GetComponent<AudioSource>();
+        Debug.Log(wallSoundEffect);
+        trapSoundEffect = transform.GetChild(2).GetComponent<AudioSource>();
+        ennemiSoundEffect = transform.GetChild(3).GetComponent<AudioSource>();
+
+
     }
 
     // Update is called once per frame
@@ -47,8 +58,24 @@ public class Movement : MonoBehaviour
         if(collision.CompareTag("Pico"))
         {
             GameManager.instance.PlayerTakeDammage();
+            trapSoundEffect.Play();
         }
-        
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Wall")
+        {
+            wallSoundEffect.Play();
+        }
+
+        if (collision.collider.tag == "Ennemy")
+        {
+            ennemiSoundEffect.Play();
+        }
+
+
     }
 
     
