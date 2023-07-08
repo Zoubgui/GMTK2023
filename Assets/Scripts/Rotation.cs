230de87652412a1d338f53bebbf1d6828ae02761
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2023-07-08 23:22:43 +0200

    LVL356

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed;
+
+    private float rotationMovement = 1;
+    Vector3 rotation;
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation = new Vector3(0, 0, rotationSpeed);
+        transform.Rotate(rotation*Time.fixedDeltaTime);
+    }
+}
