976671b42895f5d36a2483bac8d201b28585ac7c
Zoubgui <gui.home.ehrhardt@gmail.com>
2023-07-08 16:37:47 +0200

    Cinématique

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScript : MonoBehaviour
+{
+    int sceneActuelleIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sceneActuelleIndex = SceneManager.GetActiveScene().buildIndex;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneActuelleIndex + 1);
+        }
+    }
+}
